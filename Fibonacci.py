898c85b2c727867dc82e0d458dd7d98af3015b4a
temor01 <134977930+temor01@users.noreply.github.com>
2024-01-17 00:21:49 -0700

    trying to make it work, no success yet

--- Fibonacci.py
+++ Fibonacci.py
@@ -8,3 +8,44 @@ num_2 = int(input("Please input another number: "))
 choice_num = list(range(num_1, num_2))
 
 print(choice_num)
+
+result = list()
+
+n = choice_num[-1]
+
+def fib():
+    while i != n:
+    
+        for i in range(choice_num):
+            i = i + choice_num[i]
+            result.append(i)
+
+print(result)
+
+print(n)
+
+# Program to display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
