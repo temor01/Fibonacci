046b63e0d77d0d693e6344ca6865c3452cca9bef
temor01 <134977930+temor01@users.noreply.github.com>
2024-01-17 00:08:42 -0700

    start making values, and prepare parameters for using later

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,3 +2,9 @@
 I will attempt to make a program that can do Fibonacci sequence of any given two numbers.
 Wish me luck for I will a lot."""
 
+num_1 = int(input("Please input a number: "))
+num_2 = int(input("Please input another number: "))
+
+choice_num = list(range(num_1, num_2))
+
+print(choice_num)
