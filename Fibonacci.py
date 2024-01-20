e642bc48c4fd4526c25eb702bf89935dc00578b3
temor01 <134977930+temor01@users.noreply.github.com>
2024-01-19 23:15:58 -0700

    trying some toturials, not working so far

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,8 +2,17 @@
 I will attempt to make a program that can do Fibonacci sequence of any given two numbers.
 Wish me luck for I will a lot."""
 
-num = int(input("Please input the number of terms: ")
+num = int(input("Please enter the number of terms: "))
 
-print(nums)
-
-
+def fibonacciSeries(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacciSeries(num-1) + fibonacciSeries(num-2)
+    
+n = int(input())
+print("Fibonacci series:", end='')
+for n in range(0, n):
+    print(fibonacciSeries(n), end='')
